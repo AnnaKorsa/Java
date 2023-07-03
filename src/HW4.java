2ace30f877399ba562adc71c66c2a5cd3df536e5
Anna Kors <ankors320@gmail.com>
2023-07-03 23:52:17 +0300

    Done

---
+++ src/HW4.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class HW4 {
+    public static void main(String[] args) {
+//        reverseList(createList(15,0,20));
+        task0();
+
+    }
+//    Задание
+//
+//  1) Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+
+    static void reverseList(LinkedList<Integer> list) {
+        Collections.reverse(list);
+        System.out.println("\"Перевернутый\" список: "+list);
+    }
+
+    static LinkedList<Integer> createList(int size, int min, int max) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < size; i++) {
+            list.add ((int) (Math.random() * (max - min + 1) + min));
+        }
+        System.out.println("Начальный список: "+list);
+        return list;
+    }
+
+
+
+
+
+//    2) Реализуйте очередь с помощью LinkedList со следующими методами:
+//    enqueue() - помещает элемент в конец очереди,
+//    dequeue() - возвращает первый элемент из очереди и удаляет его,
+//    first() - возвращает первый элемент из очереди, не удаляя.
+static void task0(){
+
+        MyQueue myQueue=new MyQueue();
+    System.out.println("Дана очередь: "+ myQueue.creatQueue());
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        myQueue.enqueue(3);
+        myQueue.enqueue(4);
+        myQueue.enqueue(5);
+        myQueue.print();
+        myQueue.deQueue();
+        myQueue.print();
+        myQueue.first();
+        myQueue.print();
+}
+}
---
+++ src/MyQueue.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class MyQueue {
+    private Queue<Integer> list = new LinkedList<>();
+    Random rand = new Random();
+    private int size = rand.nextInt(1, 10);
+
+    Queue<Integer> creatQueue() {
+        for (int i = 0; i < size; i++) {
+            list.add(rand.nextInt(1, 100));
+        }
+        return list;
+    }
+
+    boolean isEmpty() {
+        return list == null;
+    }
+
+    void enqueue(int item) {
+        list.add(item);
+    }
+
+
+    void deQueue() {
+        if (list.isEmpty()) {
+            System.out.println("Очередь пуста!");
+        }else {
+            int firstDel = list.poll();
+            System.out.println("Первый элемент очереди,который будет удален: " + firstDel);
+        }
+
+    }
+
+    void first() {
+        if (list.isEmpty()) {
+            System.out.println("Очередь пуста!");
+        } else {
+            int firstEl = list.peek();
+            System.out.println("Первый элемент очереди: " + firstEl);
+        }
+    }
+
+        void print() {
+            if (list.isEmpty()) {
+                System.out.println("Очередь пуста!");
+            }else {
+                System.out.println("В даный момент очередь имеет вид: " + list);
+            }
+    }
+}
+
+
